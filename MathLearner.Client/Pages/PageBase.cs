ec00f2e334b04401f3cf3fcd05cd5706871f77d7
Paulino Auyon <paulinoauyon@outlook.com>
2023-11-07 09:30:03 -0800

    Move grid page items into components

---
+++ MathLearner.Client/Pages/PageBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MathLearnerWasmApp.Pages
+{
+    public class PageBase : ComponentBase
+    {
+        protected string PageTitle = string.Empty;
+        protected bool IsPageLoading = true;
+    }
+}
